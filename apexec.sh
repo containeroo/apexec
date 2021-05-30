16631a4047f77657c846027885234779cd7ea66f
gi8 <giottolino@gmail.com>
2021-05-30 22:29:19 +0200

    add VAULT_PASSWORD_FILE, change log dir

--- apexec.sh
+++ apexec.sh
@@ -11,22 +11,25 @@ Usage: apexec.sh PLAYBOOK_URL
                  SSH_USER
                  SLACK_TOKEN
                  SLACK_CHANNEL
+                 VAULT_PASSWORD_FILE
                  [-h|--help]
 
 Helper script for https://github.com/adnanh/webhook.
 
-All arguments are positional arguments!
+All arguments are positional!
   "
 }
 
 function init {
   [ ! -d /tmp/apexec ] && mkdir -p /tmp/apexec
-  WORK_DIR="/tmp/apexec/$(pwgen 6 1)"
+  JOB_ID=$(pwgen 6 1)
+  WORK_DIR=/tmp/apexec/${JOB_ID}
   PLAYBOOK_URL=${1}
   PLAYBOOK_FILE=${2}
   SSH_USER=${3}
   SLACK_TOKEN=${4}
   SLACK_CHANNEL=${5}
+  VAULT_PASSWORD_FILE=${6}
 
   [ -z "${PLAYBOOK_URL}" ] && \
     echo "environment variable 'PLAYBOOK_URL' not set!" && \
@@ -39,6 +42,12 @@ function init {
   [ -z "${SSH_USER}" ] && \
     echo "environment variable 'SSH_USER' not set!" && \
     exit 1
+
+  [ -n "${VAULT_PASSWORD_FILE}" ] && \
+    VAULT_PASSWORD_FILE="--vault-password-file ${VAULT_PASSWORD_FILE}"
+
+  PLAYBOOK_NAME=${PLAYBOOK_URL##*/}
+  PLAYBOOK_NAME=${PLAYBOOK_NAME%.git}
 }
 
 function pull_playbook {
@@ -51,7 +60,7 @@ function install_requirements {
 }
 
 function execute_ansible_playbook {
-  ansible-playbook ${PLAYBOOK_FILE} --diff --extra-vars=ansible_user=${SSH_USER} --vault-password-file ~/tmp/ansible-vault-pass &> log.txt
+  ansible-playbook ${PLAYBOOK_FILE} --diff --extra-vars=ansible_user=${SSH_USER} ${VAULT_PASSWORD} &> /tmp/${PLAYBOOK_NAME}-${JOB_ID}.log
 }
 
 function send_notification {
@@ -62,11 +71,9 @@ function send_notification {
     echo "environment variable 'SLACK_TOKEN' not set" && \
     return
 
-  summary=$(sed -n '/PLAY RECAP .*/ { :a; n; p; ba; }' log.txt | sed -r '/^\s*$/d')
-  playbook_name=${PLAYBOOK_URL##*/}
-  playbook_name=${playbook_name%.git}
-  echo -e "PLAY RECAP:\n${summary}\n$(cat log.txt)" > log.txt
-  curl -F file=@log.txt -F "initial_comment=Ansible Playbook execution: ${playbook_name}" -F "channels=#${SLACK_CHANNEL}" -H "Authorization: Bearer ${SLACK_TOKEN}" https://slack.com/api/files.upload
+  summary=$(sed -n '/PLAY RECAP .*/ { :a; n; p; ba; }' /tmp/${PLAYBOOK_NAME}-${JOB_ID}.log | sed -r '/^\s*$/d')
+  echo -e "PLAY RECAP:\n${summary}\n$(cat /tmp/${PLAYBOOK_NAME}-${JOB_ID}.log)" > /tmp/${PLAYBOOK_NAME}-${JOB_ID}.log
+  curl -F file=@/tmp/${PLAYBOOK_NAME}-${JOB_ID}.log -F "initial_comment=Ansible Playbook execution: ${PLAYBOOK_NAME}" -F "channels=#${SLACK_CHANNEL}" -H "Authorization: Bearer ${SLACK_TOKEN}" https://slack.com/api/files.upload
 }
 
 function cleanup {
